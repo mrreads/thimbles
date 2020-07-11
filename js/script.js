2599bad6ae4c77c0376f2634aa51fe6bac5b6135
Alexander <mrreads@yandex.com>
2020-07-12 02:33:47 +0300

    stylize

---
+++ js/script.js
@@ -0,0 +1,48 @@
+﻿var listObject = [];
+var globalCounter = 0;
+
+class Cup 
+{
+    constructor(id) 
+    {
+        document.querySelector('#wrapper').innerHTML += '<div id="' + id + '" class="cup"> </div>';
+        listObject[globalCounter] = id;
+        globalCounter++;
+    }
+}
+
+let cupOne = new Cup('cupOne');
+let cupTwo = new Cup('cupTwo');
+let cupThree = new Cup('cupThree');
+let cupFour = new Cup('cupFour');
+let cupFive = new Cup('cupFive');
+let cupSix = new Cup('cupSix');
+let cupSeven = new Cup('cupSeven');
+let cupEight = new Cup('cupEight');
+let cupTen = new Cup('cupTen');
+let cupNine = new Cup('cupNine');
+let cupEleven = new Cup('cupEleven');
+let cupTwelwe = new Cup('cupTwelwe');
+
+let randomNumber = Math.round(Math.random() * (globalCounter - 1));
+document.getElementById(listObject[randomNumber]).innerHTML = '<div class="ball"> </div>';
+
+for (let i = 0; i < globalCounter; i++) 
+{
+    if (i == randomNumber) 
+    {
+        document.getElementById(listObject[i]).addEventListener('click', function () 
+        {
+            alert('Вы победили');
+            location.reload(true);
+        });
+    } 
+    else 
+    {
+        document.getElementById(listObject[i]).addEventListener('click', function () 
+        {
+            alert('Вы проиграли');
+            location.reload(true);
+        });
+    }
+}
